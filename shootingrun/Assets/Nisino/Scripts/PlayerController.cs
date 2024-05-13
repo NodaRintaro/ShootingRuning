088168ac4eeed8d2d26003fd62bbd7d4dcf26eec
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2024-05-13 20:04:29 +0900

    ジャンプできるようにした

--- shootingrun/Assets/Nisino/Scripts/PlayerController.cs
+++ shootingrun/Assets/Nisino/Scripts/PlayerController.cs
@@ -1,31 +1,34 @@
 using System;
 using UnityEngine;
-[RequireComponent(typeof(Rigidbody2D))]
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public int Hp {private get; set; }
+    public int Hp { private get; set; }
     Rigidbody2D _rb;
     [SerializeField] float _jumpPower;
     Vector2 _jumpVector;
     bool _isGround;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _jumpVector = new Vector2(0, _jumpPower);
+        _jumpVector = new Vector2(0, _jumpPower * 5);
+        _isGround = true;
     }
-    
+
     void Update()
     {
         if (_isGround)
         {
-            if (Input.GetButtonDown("Jump"))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                _rb.AddForce(_jumpVector,ForceMode2D.Impulse);
+                _rb.AddForce(_jumpVector, ForceMode2D.Impulse);
+                _isGround = false;
             }   
         }
     }
-    
+
     public void HpValueChange(int value)
     {
         Hp += value;
@@ -33,14 +36,20 @@ public class PlayerController : MonoBehaviour
 
     public void JumpPowerValueChange(int value)
     {
-        _jumpVector = new Vector2(0, value);
+        _jumpVector = new Vector2(0, value * 5);
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (CompareTag("Enemy"))    //Enemyタグが付いたオブジェクトに触れたらHpを減らす
+        if (collision.gameObject.CompareTag("Enemy")) //Enemyタグが付いたオブジェクトに触れたらHpを減らす
         {
             HpValueChange(-1);
+        }
+        else if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGround = true;
+
+            Debug.Log("押された");
         }
     }
 }

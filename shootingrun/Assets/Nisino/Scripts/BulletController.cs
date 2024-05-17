4d8bc63e0bb526f5393e25a4c2ad968885ba541e
Enulu <enuarle8@icloud.com>
2024-05-18 00:27:41 +0900

    Bullet

--- shootingrun/Assets/Nisino/Scripts/BulletController.cs
+++ shootingrun/Assets/Nisino/Scripts/BulletController.cs
@@ -4,13 +4,6 @@ public class BulletController : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
-        {
-            //エネミーのHPを減らす処理を追加する
-        }
-        if (!other.gameObject.CompareTag("Player"))
-        {
-            Destroy(gameObject);   
-        }
+        
     }
 }

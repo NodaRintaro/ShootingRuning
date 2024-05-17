5283877bea6f7b415b79a82f5e65739b7bb51457
Enulu <enuarle8@icloud.com>
2024-05-18 01:47:10 +0900

    [fix]Manager

--- shootingrun/Assets/Noda/HPManager.cs
+++ shootingrun/Assets/Noda/HPManager.cs
@@ -1,43 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Singleton;
 using UnityEngine;
 
-class HPManager : MonoBehaviour
+class HPManager : SingletonMonoBehaviour<HPManager>
 {
-<<<<<<< HEAD
     GameManagerSample _gameManagerSample;
-=======
-    private TimeManager tm;
-    private ScoreManager sm;
->>>>>>> 39d6494ca484e019e2747ab168a1129620390377
+    
     /// <summary>
     /// プレイヤーのライフポイント
     /// </summary>
     public int _hp = 5;
+
+    public float time;
     void Start()
     {
-<<<<<<< HEAD
         _gameManagerSample = GameManagerSample.GetInstancs;
         _hp = _gameManagerSample.Life;
-=======
-        tm = GetComponent<TimeManager>();
-        sm = GetComponent<ScoreManager>();
-        _hp = 3;
->>>>>>> 39d6494ca484e019e2747ab168a1129620390377
+        time = _gameManagerSample.Time;
     }//hpの初期化
 
     void Damage(int damage)
     {
         _hp -= damage;
-<<<<<<< HEAD
-=======
-        if (_hp <= 0)
+        if (_hp == 0)
         {
-            float m = (tm.min * 60);
-            tm._finishTime = tm.second + m;
-            int i = (int)tm._finishTime;
-            sm.Score += i;
-        }//HPが０になったらタイムをScoreに加算
->>>>>>> 39d6494ca484e019e2747ab168a1129620390377
+            int _time = (int)time;
+            ScoreManager.Instance.Hit(_time);
+        }
     }//ダメージを受けたときにhpを減らす
 }

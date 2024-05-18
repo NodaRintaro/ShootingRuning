f806813cab4a67e6a4633826fa66fc8b541ddc2b
takasimamasahiro <134473651+takasimamasahiro@users.noreply.github.com>
2024-05-18 23:45:47 +0900

    [update]　当たり判定追加

--- shootingrun/Assets/takasima/tesuto/UpDwnEnemey.cs
+++ shootingrun/Assets/takasima/tesuto/UpDwnEnemey.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class UpDwnEnemey : MonoBehaviour
 {
     [SerializeField] float _yMoveWidth = 1.5f;

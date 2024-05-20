c316da8845488aa9a0d5f3292b7c7dbe7f7381cf
Kitamura Haruki <90311063+629Kitamura@users.noreply.github.com>
2024-05-20 13:47:30 +0900

    [add.fix]GameManagerSampleにGameoverメソッドを追加、_gameoverFlag変数の追加。 ResultUIプレハブの追加

--- shootingrun/Assets/Kitamura/GameManagerSample.cs
+++ shootingrun/Assets/Kitamura/GameManagerSample.cs
@@ -8,11 +8,12 @@ public class GameManagerSample : MonoBehaviour
 {
     static GameManagerSample instance;
 
-    private void Awake()
+    void Awake()
     {
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
         }
     }
 
@@ -24,18 +25,47 @@ public class GameManagerSample : MonoBehaviour
         }
     }
 
-    public int Life { get => life; set => life = value; }
-    public int MaxLife { get => maxLife; set => maxLife = value; }
-    public int Score { get => score; set => score = value; }
-    public float Time { get => time; set => time = value; }
+    public int Life { get => _life; set => _life = value; }
+    public int MaxLife { get => _maxLife; set => _maxLife = value; }
+    public int Score { get => _score; set => _score = value; }
+    public float Time { get => _time; set => _time = value; }
+    public int ResultScore { get => _resultScore; }
+    public bool GameoverFlag { get => _gameoverFlag; }
 
-    [SerializeField,Range(0,10)] int life = 3;
-    [SerializeField] int maxLife = 10;
-    [SerializeField] int score = 100000;
-    [SerializeField] float time = 0;
+    [SerializeField,Range(0,10)] int _life = 3;
+    [SerializeField] int _maxLife = 10;
+    [SerializeField] int _score = 100000;
+    [SerializeField] float _time = 0;
+    int _resultScore = 0;
+    [SerializeField] GameObject _resultUI;
+    bool _gameoverFlag = false;
+
+    private void Start()
+    {
+        _resultUI.SetActive(false);
+    }
 
     void Update()
     {
-        Time += UnityEngine.Time.deltaTime;
+        if (Life <= 0) 
+        {
+            _gameoverFlag = true;
+        }
+        else
+        {
+            _time += UnityEngine.Time.deltaTime;
+        }
+
+        if (GameoverFlag) 
+        {
+            Gameover();
+            _gameoverFlag = false;
+        }
+    }
+
+    void Gameover()
+    {
+        _resultScore = _score + (int)(_time * 100);
+        _resultUI.SetActive(true);
     }
 }
